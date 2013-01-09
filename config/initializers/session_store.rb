e741c5fd85344ab460e8d542b8582bedf2596034
greyfox <greyfox@bshellz.pl>
2013-01-09 11:20:27 +0100

    Rails update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-DesktopjsRails::Application.config.session_store :cookie_store, key: '_desktopjs-rails_session'
+DesktopjsRails::Application.config.session_store :cookie_store, key: '_desktopjs_rails_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
